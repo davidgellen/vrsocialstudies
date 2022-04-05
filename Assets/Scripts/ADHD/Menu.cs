abff0e040e1028e17ce14b5f48ac8ed023e1fb4b
PatrikK <74940369+patrik-kupculak@users.noreply.github.com>
2022-04-05 16:37:03 +0200

    Added adhd menu
    :8ball:

---
+++ Assets/Scripts/ADHD/Menu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject renderCharacter; //Show character
+    public GameObject[] characters; //Adding all characters
+
+    public void SelectCharacter(int selection)
+    {
+        if (selection < characters.Length)
+        {
+            for(int i = 0; i < characters.Length; i++)
+            {
+                if (i == selection)
+                    characters[i].SetActive(false);
+                else
+                    characters[i].SetActive(true);
+            }
+            PlayerPrefs.SetInt("choosen_avatar", selection); //PlayerPrefs.GetInt("choosen_avatar");
+        }
+    }
+
+    public void LoadADHD()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Update()
+    {
+        renderCharacter.transform.RotateAround(renderCharacter.transform.position, renderCharacter.transform.up, Time.deltaTime * 15f); //Rotation object for better vizualization
+    }
+}
